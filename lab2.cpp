712a5ce542cc3a8b95121ae1cc79e5aaafdcc95f
Staylor72 <kirimarakaev@gmail.com>
2026-02-12 10:01:48 +0300

    Add files via upload

---
+++ lab2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    //SetConsoleOutputCP(CP_UTF8);
+    setlocale(LC_ALL, "russian");
+    const int n = 10;
+    float m[n] = {};
+
+    for (int q = 0; q < 10; q++)
+    {
+        m[q] = rand() % 10;
+    }
+
+    cout << "задание a) - Умножить на 2:" << endl;
+
+    int i = 0;
+
+    while (i < 5)
+    {
+        m[i] *= 2;
+        cout << m[i] << endl;
+        i++;
+    }
+
+    i = 0;
+
+    int A;
+    cout << "Введите число A" << endl;
+    cin >> A;
+    cout << "задание б) - Уменьшить на А:" << endl;
+    while (i < 5)
+    {
+        m[i] -= A;
+        cout << m[i] << endl;
+        i++;
+    }
+
+
+    i = 0;
+    float x = m[0];
+    cout << "задание в) - Разделить на 1 элемент" << endl;
+    while (i < 5)
+    {
+        m[i] = m[i] / x;
+        cout << m[i] << endl;
+        i++;
+    }
+
+    return 0;
+}

7281feb884a5e74d93610278a0b188564f1cc8c4
Staylor72 <kirimarakaev@gmail.com>
2026-03-05 10:42:50 +0300

    Add files via upload
    lab 5

---
+++ lab5_1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<windows.h>
+using namespace std;
+
+int sumary(int *ptr,int len)
+{
+    int sum = 0;
+    for(int i{0}; i < len; i++)
+    {
+        sum += *(ptr + i);
+    }
+    return sum;
+}
+
+int main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+    const int n = 3;
+    int mass[n];
+    for(int i{0}; i < n; i++)
+    {
+        *(mass + i) = rand()%100;
+        cout << *(mass + i) << endl;
+    }
+    cout << sumary(mass, n);
+}

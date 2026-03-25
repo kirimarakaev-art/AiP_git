40f81d37be314fc875871bf9c68643a96abaff33
Staylor72 <kirimarakaev@gmail.com>
2026-03-25 23:25:16 +0300

    lab7 \n final

--- 2semestr/lab7_all.cpp
+++ 2semestr/lab7_all.cpp
@@ -1,14 +1,46 @@
 #include <iostream>
+#include <math.h>
 #include <windows.h>
-using namespace std;
+#include <ctime>
 
-int main() {
+using namespace std;
+int main()
+{
     SetConsoleOutputCP(CP_UTF8);
-    int len;
-    cin >> len;
-    int *mass = new int[len];
-    for (int i = 0; i < len ;i++) mass[i] = rand() % 10;
+    srand(time(0));
+    int m, n;
+    cout << "Введите размер массива:" << endl; 
+    cin >> m >> n;
+    int **a = new int *[m];
     
-    delete [] mass;
+    for (int i=0;i<m;i++)
+    {
+        a[i]= new int [n];
+        for (int j=0;j<n;j++)
+        {
+            a[i][j]=rand()%100;
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    int min = **a;
+    int count = 0;
+    for (int j=0; j < m;j++)
+    {
+        for (int i=0;i<n;i++)
+        {
+            if (a[i][j] < min)
+            {
+                min = a[i][j];
+                count = j;
+            }
+        }
+       
+    }
+    cout << "Минимальный элемент массива: " << min << endl;
+    cout << "Номер столбца: " << count << endl; 
+
+    delete []a;
     return 0;
 }

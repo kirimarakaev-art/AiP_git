5031620c9d4fef839be0f192ad548bc5c7780e69
Staylor72 <kirimarakaev@gmail.com>
2026-03-31 20:47:23 +0300

    lab 9:

---
+++ 2semestr/lab9.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <windows.h>
+#include <cstdlib>
+using namespace std;
+
+struct list {
+    int field;
+    struct list *next;
+    struct list *prev;
+};
+
+list* init(int a) {
+    list *lst = new list;
+    lst->field = a;
+    lst->next = NULL;
+    lst->prev = NULL;
+    return lst;
+}
+
+list* add(list *lst, int number) {
+    list *temp = new list;
+    list *p = lst->next;
+    lst->next = temp;
+    temp->field = number;
+    temp->next = p;
+    temp->prev = lst;
+    if (p != NULL) p->prev = temp;
+    return temp;
+}
+
+void del(list *head) {
+    if (head == NULL) return;
+
+    list *p = head;
+    while (p->next != NULL) {
+        p = p->next;
+    }
+
+    if (p->prev != NULL) {
+        list *prevNode = p->prev;
+        prevNode->next = NULL;
+    }
+
+    delete p; 
+}
+
+void print(list *lst) {
+    list *p = lst;
+    while (p != NULL) {
+        cout << p->field << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
+
+int main(){
+    SetConsoleOutputCP(CP_UTF8);
+
+    list *root = init(10);
+    list *n2 = add(root, 20);
+    list *n3 = add(n2, 30);
+
+    cout << "Список до удаления:" << endl;
+    print(root);
+    del(root);
+    cout << "Список после удаления:" << endl;
+    print(root);
+
+    return 0;
+}

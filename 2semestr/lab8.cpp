0d36871c04c1741d969b816ed9e8334ff02ffa40
Staylor72 <kirimarakaev@gmail.com>
2026-04-02 10:08:53 +0300

    Update lab8.cpp

--- 2semestr/lab8.cpp
+++ 2semestr/lab8.cpp
@@ -4,12 +4,43 @@ using namespace std;
 
 struct item
 {
-char name [10];
-item *next;
+    char name [10];
+    item *next;
 };
+
+item *Add(item *head, char newname[10])
+{
+    item *tmp = new item;  
+    if (head==0)  
+    {
+        strcpy(tmp->name, newname);  
+        tmp -> next = 0;
+        return tmp;  
+    }
+    else  
+    {  
+        strcpy(tmp->name, newname); tmp->next = head;
+        head=tmp;
+        return head;
+    }
+}
+void Print (item *head)  
+{  
+    item *x;  
+    x=head;  
+    while (x!=0)  
+    {  
+        cout<<x->name<<'\n';  
+        x=x->next;
+    }  
+}
 
 int main() {
     SetConsoleOutputCP(CP_UTF8);
-    
+    item *head = 0;
+    head = Add(head,"Ibragimova");
+    head = Add(head,"Medvedev");
+    head = Add(head,"Zubarev");
+    Print(head);  
     return 0;
 }
